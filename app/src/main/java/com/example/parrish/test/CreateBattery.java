10f4c69f2e813e8a775092a45436197bd46b969b
RichardGamble <richardgamble@live.com>
2015-06-15 16:35:22 -0400

    added create_create battery activity added main activity

---
+++ app/src/main/java/com/example/parrish/test/CreateBattery.java
@@ -0,0 +1,55 @@
+package com.example.parrish.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class CreateBattery extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_battery);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_create_battery, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void onSubmitClick(View view) {
+        EditText txtBatteryName = (EditText) findViewById(R.id.txtBatteryName);
+        EditText txtBatteryCells = (EditText) findViewById(R.id.txtBatteryCells);
+        EditText txtBatteryMah = (EditText) findViewById(R.id.txtMah);
+
+        String batteryName;
+        String batteryCells;
+        String batteryMah;
+        //test git push
+        batteryName = txtBatteryName.getText().toString();
+        batteryCells = txtBatteryCells.getText().toString();
+        batteryMah = txtBatteryMah.getText().toString();
+
+
+    }
+}
